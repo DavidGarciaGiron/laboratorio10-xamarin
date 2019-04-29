9567d424f9d361022371836287e78c9f39739287
unknown <david.garcia@tecsup.edu.pe>
2019-04-29 18:02:12 -0500

    first commit

---
+++ laboratorio10/laboratorio10/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace laboratorio10
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			btnViewToViewDemo.Clicked += async (sender, e) =>
+			{
+				await Navigation.PushAsync(new ViewToViewDemo());
+			};
+			btnBindingModeDemo.Clicked += async (sender, e) =>
+			{
+				await Navigation.PushAsync(new BindingModeDemo());
+			};
+			btnListViewDemo.Clicked += async (sender, e) =>
+			{
+				await Navigation.PushAsync(new ListViewDemo());
+			};
+			btnPickerDemo.Clicked += async (sender, e) =>
+			{
+				await Navigation.PushAsync(new PickerDemo());
+			};
+		}
+	}
+}
